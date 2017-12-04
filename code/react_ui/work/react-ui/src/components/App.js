fbc80d33ba1627723d9d94c4ae8565eb8724f7c5
zhiyong <13852292775@163.com>
2017-12-04 09:14:36 +0800

    'aa'

--- code/react_ui/work/react-ui/src/components/App.js
+++ code/react_ui/work/react-ui/src/components/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Button, DatePicker } from 'antd';
-// import 'antd/dist/antd.css';
 
-class App extends React.Component {
-  render () {
+class App extends React.Component{
+  render(){
     return (
       <div>
-        <Button type="primary">Primary</Button>
-        <Button>Default</Button>
-        <Button type="dashed">Dashed</Button>
-        <Button type="danger">Danger</Button>
-        <DatePicker />
+
       </div>
-    );
+    )
   }
 }
-
-
-export default App;
